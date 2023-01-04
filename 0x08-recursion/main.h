4ed0bf72f9bb44c92a4609aaa81532f6c51642e8
Dpreshy <officialpresh20@gmail.com>
2023-01-04 07:02:03 -0800

    try

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,17 @@
+#ifndef MAIN_H_INCLUDED
+
+#define MAIN_H_INCLUDED
+
+
+
+/**
+ *
+ *  * void prototypes()
+ *
+ *   * int prototypes()
+ *
+ *    */
+
 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -6,3 +20,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+
+#endif /* MAIN_H_INCLUDED */
